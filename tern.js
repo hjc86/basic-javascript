f95d0b63aa21595141f813b80bcd83309d38937b
jamesabustan <jamesabustan04@gmail.com>
2020-03-24 16:11:44 +0000

    added files

---
+++ tern.js
@@ -0,0 +1,30 @@
+console.log('ITEM COUNT')
+
+let item;
+let count;
+
+// function itemCount(item, count){
+//     if (count > 1){
+//         console.log(`I have ${count} ${item}s`);
+//     } else {`I have ${count} ${item}`}   
+// };
+
+function itemCount(item, count){
+    (count > 1) ? console.log(`I have ${count} ${item}s`) :
+    console.log(`I have ${count} ${item}`) ;  
+};
+
+itemCount('peas', 4);
+
+console.log('PATTERNS EXERCISE')
+
+let lineOne = '& & & & &';
+let lineTwo= ' & & & & & ';
+
+for (let i=1 ; i <= 10; i++){
+    if (i % 2 !== 0){
+        console.log(lineOne);
+    } else {console.log(lineTwo);}
+
+    (i % 2 !== 0) ? console.log(lineOne): console.log(lineTwo);
+}
